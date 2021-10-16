e775a9ccea02e87fe89ddcee42afc46c6f7d9189
unknown <slisette127@gmail.com>
2021-10-15 22:27:51 -0500

    added MUI

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+
 // import { useParams } from "react-router-dom"
-import cars from '../cars.json'
+import cars from '../cars.json';
 
 // import material ui components here //
+import { Card,CardContent, Chip, Paper } from '@material-ui/core';
 // Container, Paper, Chip //
 
 const Car = (props) => {
@@ -11,7 +13,21 @@ const Car = (props) => {
     console.log(car)
     return (
         <div>
-            {/* <span>{car.Name}</span> */}
+            <Card style={{ minWidth: 275,
+                    maxWidth:500,
+                    }}>
+            <h1><span>{car.Name}</span></h1>
+                <CardContent>
+                    <Chip label={car.id}></Chip>
+                    <Chip label={car.Name}></Chip>
+                    <Chip label={car.Miles_per_Gallon}></Chip>
+                    <Chip label={car.Cylinders}></Chip>
+                    <Chip label={car.Displacement}></Chip>
+        
+                </CardContent>
+      
+            </Card>
+            
         </div>
     )
 }
